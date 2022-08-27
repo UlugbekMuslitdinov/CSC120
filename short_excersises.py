1b48494ed0243f7e65ac59d7efaa48270e406408
Ulugbek <ulugbekmuslitdinov@gmail.com>
2022-08-27 14:07:01 -0700

    num2letter

---
+++ short_excersises.py
@@ -0,0 +1,10 @@
+def number2letter(n):
+    """
+    Convert a number to a letter.
+
+    Parameters: n (int) from 0 to 25
+
+    Returns: the letter (char) corresponding to n
+    """
+    letters = "abcdefghijklmnopqrstuvwxyz"
+    return letters[n]
