3a492a631eafdb9304f19cec449ae9209fa425f5
Ulugbek <ulugbekmuslitdinov@gmail.com>
2022-08-27 15:21:04 -0700

    concat_elements2

--- short_excersises.py
+++ short_excersises.py
@@ -34,5 +34,26 @@ def concat_elements(slist, startpos, stoppos):
         startpos += 1
     return return_string
 
-print(concat_elements(['aa','bb','cc','dd'], 1, 3))
+
+def concat_elements2(slist, startpos, stoppos):
+    """
+    Concatenate elements of a list.
+
+    Parameters: slist (list)
+                startpos (int)
+                stoppos (int)
+    Returns: the concatenated string
+    """
+    # If no arguments are given, return None
+    if not slist or not startpos or not stoppos:
+        return None
+    if stoppos < startpos:
+        return ""   # Return empty string if startpos is greater than stoppos
+    if startpos < 0:
+        startpos = 0    # Set startpos to 0 if it is less than 0
+    if stoppos > len(slist):
+        stoppos = len(slist)    # Set stoppos to the length of the list if it is greater than the length of the list
+    return_string = "".join(slist[startpos:stoppos+1])
+
+    return return_string
 
