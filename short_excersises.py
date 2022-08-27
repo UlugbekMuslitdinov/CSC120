ada6d720527399106662e8f8b178d5c8da2e0997
Ulugbek <ulugbekmuslitdinov@gmail.com>
2022-08-27 15:07:15 -0700

    concat1

--- short_excersises.py
+++ short_excersises.py
@@ -8,3 +8,31 @@ def number2letter(n):
     """
     letters = "abcdefghijklmnopqrstuvwxyz"
     return letters[n]
+
+
+def concat_elements(slist, startpos, stoppos):
+    """
+    Concatenate elements of a list.
+
+    Parameters: slist (list)
+                startpos (int)
+                stoppos (int)
+    Returns: the concatenated string
+    """
+    # If no arguments are given, return None
+    if not slist or not startpos or not stoppos:
+        return None
+    if stoppos < startpos:
+        return ""   # Return empty string if startpos is greater than stoppos
+    if startpos < 0:
+        startpos = 0    # Set startpos to 0 if it is less than 0
+    if stoppos > len(slist):
+        stoppos = len(slist)    # Set stoppos to the length of the list if it is greater than the length of the list
+    return_string = ""
+    while startpos <= stoppos:
+        return_string += slist[startpos]
+        startpos += 1
+    return return_string
+
+print(concat_elements(['aa','bb','cc','dd'], 1, 3))
+
