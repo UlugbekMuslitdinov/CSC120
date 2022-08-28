370da60bf4f12d58c37a26da130e5422c97f776f
Ulugbek <ulugbekmuslitdinov@gmail.com>
2022-08-27 17:06:15 -0700

    column2list

--- short_excersises.py
+++ short_excersises.py
@@ -23,11 +23,13 @@ def concat_elements(slist, startpos, stoppos):
     if not slist or not startpos or not stoppos:
         return None
     if stoppos < startpos:
-        return ""   # Return empty string if startpos is greater than stoppos
+        return ""  # Return empty string if startpos is greater than stoppos
     if startpos < 0:
-        startpos = 0    # Set startpos to 0 if it is less than 0
+        startpos = 0  # Set startpos to 0 if it is less than 0
     if stoppos > len(slist):
-        stoppos = len(slist)    # Set stoppos to the length of the list if it is greater than the length of the list
+        stoppos = len(
+            slist
+        )  # Set stoppos to the length of the list if it is greater than the length of the list
     return_string = ""
     while startpos <= stoppos:
         return_string += slist[startpos]
@@ -48,12 +50,27 @@ def concat_elements2(slist, startpos, stoppos):
     if not slist or not startpos or not stoppos:
         return None
     if stoppos < startpos:
-        return ""   # Return empty string if startpos is greater than stoppos
+        return ""  # Return empty string if startpos is greater than stoppos
     if startpos < 0:
-        startpos = 0    # Set startpos to 0 if it is less than 0
+        startpos = 0  # Set startpos to 0 if it is less than 0
     if stoppos > len(slist):
-        stoppos = len(slist)    # Set stoppos to the length of the list if it is greater than the length of the list
-    return_string = "".join(slist[startpos:stoppos+1])
+        stoppos = len(
+            slist
+        )  # Set stoppos to the length of the list if it is greater than the length of the list
+    return_string = "".join(slist[startpos : stoppos + 1])
 
     return return_string
 
+
+def column2list(grid, n):
+    """
+    Convert a column of a grid to a list.
+
+    Parameters: grid (list)
+                n (int)
+    Returns: the list of the column
+    """
+    return_list = []
+    for row in grid:
+        return_list.append(row[n])
+    return return_list
