07a37018adcb628ba519abc2b9b3867a692ffb8e
Ulugbek <ulugbekmuslitdinov@gmail.com>
2022-09-02 11:06:19 -0700

    ica4

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def print_keys(dictionary):
-    for key in dictionary:
-        print(key)
+d = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "nine": 9, "ten": 10}
+words = ["one", "two", "three", "four", "five", "six", "seven", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety", "hundred", "thousand", "million", "billion", "trillion", "quadrillion", "quintillion", "sextillion", "septillion", "octillion", "nonillion", "decillion", "undecillion", "duodecillion", "tredecillion", "quattuordecillion", "quindecillion", "sexdecillion", "septendecillion", "octodecillion", "novemdecillion", "vigintillion", "googol"]
 
+for value in d.values():
+    if value % 2 == 0:
+        print(value)
 
-print_keys({'a': 1, 'b': 2, 'c': 3})
+for key in words:
+    print(key in d)
