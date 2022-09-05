e30aeef895944c625cfd168bfd85f79bb1bd6729
Ulugbek <ulugbekmuslitdinov@gmail.com>
2022-09-04 19:15:47 -0700

    selection sort

--- algorithms.py
+++ algorithms.py
@@ -1,3 +1,4 @@
+# Binary sort
 lst = [i for i in range(101)]
 
 
@@ -22,3 +23,24 @@ def binary_sort(list, item):
 
 
 print(binary_sort(lst, 50))
+
+
+# Selection sort
+def findSmallest(arr):
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+    return  smallest_index
+
+
+def selectionSort(arr):
+    newArr = []
+    for i in range(len(arr)):
+        smallest = findSmallest(arr)
+        newArr.append(arr.pop(smallest))
+    return newArr
+
+print(selectionSort([5, 3, 6, 2, 1, 10, 8]))
