7d8edf1792cc201a28db1bd48c1eb04e76cac278
Ulugbek <ulugbekmuslitdinov@gmail.com>
2022-09-12 11:10:31 -0700

    basics of OOP

--- main.py
+++ main.py
@@ -1,11 +1,34 @@
-d = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "nine": 9, "ten": 10}
-words = ["one", "two", "three", "four", "five", "six", "seven", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety", "hundred", "thousand", "million", "billion", "trillion", "quadrillion", "quintillion", "sextillion", "septillion", "octillion", "nonillion", "decillion", "undecillion", "duodecillion", "tredecillion", "quattuordecillion", "quindecillion", "sexdecillion", "septendecillion", "octodecillion", "novemdecillion", "vigintillion", "googol"]
+class Counter:
+    def __init__(self, name):
+        self._name = name
+        self._count = 0
 
-for value in d.values():
-    if value % 2 == 0:
-        print(value)
+    def __str__(self):
+        return f"{self._name}: {self._count}"
 
-for key in words:
-    print(key in d)
+    def click(self):
+        self._count += 1
+
+    def count(self):
+        return self._count
 
 
+clicker = Counter("Clicker")
+clicker.click()
+clicker.click()
+print(clicker.count())
+print(clicker)
+
+transistor = Counter("Transistor")
+transistor.click()
+transistor.click()
+transistor.click()
+transistor.click()
+print(transistor.count())
+
+
+import math
+class Point:
+     def __init__(self, x, y):
+        self._x = x
+        self._y = y
