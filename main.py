166e8724542270e7c4c4980bdd8ea4cb33656f85
Ulugbek <ulugbekmuslitdinov@gmail.com>
2022-09-17 17:49:51 -0700

    temps

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
-class Word:
+class Temp:
 
-    def __init__(self, word):
-        self._word = word
+    def __init__(self, temp, unit):
+        self._temp = temp
+        self._unit = unit
 
     def __str__(self):
-        return self._word.lower()
+        return str(self._temp) + str(self._unit)
+
+    def to_Celcius(self):
+        if self._unit == 'C':
+            return self._temp
+        elif self._unit == 'F':
+            return (self._temp - 32) * 5 / 9
+        else:
+            return self._temp
 
     def __eq__(self, other):
-        if len(self._word) == len(other._word):
-            for i in self._word.lower():
-                if i not in other._word.lower():
-                    return False
-            return True
-        else:
-            return False
+        return self.to_Celcius() == other.to_Celcius()
+
+    def __lt__(self, other):
+        return self.to_Celcius() < other.to_Celcius()
+
+    def __gt__(self, other):
+        return self.to_Celcius() > other.to_Celcius()
+
+    def __ne__(self, other):
+        return self.to_Celcius() != other.to_Celcius()
+
+
