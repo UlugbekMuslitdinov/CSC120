54d7b590889e52468874068efe2f51aca695b517
Ulugbek <ulugbekmuslitdinov@gmail.com>
2022-09-17 17:17:12 -0700

    words anagrams

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
-"""
-POTATO
-"""
-
 class Word:
 
     def __init__(self, word):
-        self._word = word.strip(".!:;?-").lower()
+        self._word = word
 
     def __str__(self):
-        return "Word(" + self._word + ")"
+        return self._word.lower()
 
-    def clean_word(self, word):
-        return word.strip(".!:;?-").lower()
-
-# 2) No, reference does not contain a list. It only refers to the object's unique id
-# 3) 1 - Create a list of numbers
-#    2 - Assign the list's id to x variable
+    def __eq__(self, other):
+        if len(self._word) == len(other._word):
+            for i in self._word.lower():
+                if i not in other._word.lower():
+                    return False
+            return True
+        else:
+            return False
