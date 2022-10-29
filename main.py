bf2377016879934273cc668fd48519e6fee7eb37
Ulugbek <ulugbekmuslitdinov@gmail.com>
2022-10-28 21:53:49 -0700

    binary tree

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
-# Midnights
+# Sitcom
 
-# 1)
-#     a) 8,3,7
-#     b) 4,9,5,6
-#     c) all nodes have only one parent
-#     d) 4
-#     e) 2
-#     f) 3
-#     g) 4
+class BinaryTree:
+    def __init__(self, value):
+        self._value = value
+        self._left = None
+        self._right = None
 
-# 2) Uploaded image
+    def value(self):
+        return self._value
 
-# 3) No. It does not follow a rule of hierarchy. It is not a tree.
+    def left(self):
+        return self._left
 
-# 4) Uploaded image
+    def right(self):
+        return self._right
+
+    def insert_right(self, value):
+        if self._right is None:
+            self._right = BinaryTree(value)
+        else:
+            new_node = BinaryTree(value)
+            new_node._right = self._right
+            self._right = new_node
+
+    def insert_left(self, value):
+        if self._left is None:
+            self._left = BinaryTree(value)
+        else:
+            new_node = BinaryTree(value)
+            new_node._left = self._left
+            self._left = new_node
+
